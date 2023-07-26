c398e1e5eae5c6bc1e25bd83c53ae6e6cdb1f47f
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-07-26 18:31:45 -0400

    Add permissions for CRD for volumes and drives

--- internal/controller/memcached_controller.go
+++ internal/controller/memcached_controller.go
@@ -67,6 +67,8 @@ type DeployerReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=directpvdrives,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=directpvvolumes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
