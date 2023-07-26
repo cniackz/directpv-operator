8e0bc46d94723c5265942be160057b15d64859ee
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-07-26 19:47:21 -0400

    Add permissions for directpvdrives

--- internal/controller/memcached_controller.go
+++ internal/controller/memcached_controller.go
@@ -72,6 +72,7 @@ type DeployerReconciler struct {
 //+kubebuilder:rbac:groups=apps,resources=directpvvolumes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=directpv.min.io,resources=directpvvolumes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=directpv.min.io,namespace=directpv,resources=directpvdrives,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
