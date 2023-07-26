324fa33f2f83517d0b64a5458ee77f21a0007632
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-07-26 17:31:16 -0400

    Adding node server container

--- internal/controller/memcached_controller.go
+++ internal/controller/memcached_controller.go
@@ -453,7 +453,53 @@ func (r *DeployerReconciler) daemonSetForDeployer(
 					Containers: []corev1.Container{
 						{
 							Image:           controllerImage,
-							Name:            "controller",
+							Name:            "node-server",
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							SecurityContext: &corev1.SecurityContext{
+								Privileged: &[]bool{true}[0],
+							},
+							Ports: []corev1.ContainerPort{{
+								ContainerPort: 30443,
+								Name:          "readinessport",
+							},
+								{
+									ContainerPort: 9898,
+									Name:          "healthz",
+								},
+							},
+							Args: []string{
+								"controller",
+								"--identity=directpv-min-io",
+								"-v=3",
+								"--csi-endpoint=$(CSI_ENDPOINT)",
+								"--kube-node-name=$(KUBE_NODE_NAME)",
+								"--readiness-port=30443",
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "CSI_ENDPOINT",
+									Value: "unix:///csi/csi.sock",
+								},
+								{
+									Name: "KUBE_NODE_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											APIVersion: "v1",
+											FieldPath:  "spec.nodeName",
+										},
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "socket-dir",
+									MountPath: "/csi",
+								},
+							},
+						},
+						{
+							Image:           controllerImage,
+							Name:            "node-controller",
 							ImagePullPolicy: corev1.PullIfNotPresent,
 							SecurityContext: &corev1.SecurityContext{
 								Privileged: &[]bool{true}[0],
